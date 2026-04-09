2d220ff939fd0ff8feb9cf74b1a40071b2395e42
fulstock <fulstocky@gmail.com>
2026-04-09 11:10:08 +0000

    test fix

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 python gliner_inference.py \
       --input /home/student1/data/NEREL-2part-merged-gliner/test_w384.json  \
-      --model ./models/gliner-nerel-2part-windowed-finetuned/checkpoint-1500 \
-      --metrics_output metrics-nerel-part2.json
+      --model urchade/gliner_multi-v2.1 \
+      --metrics_output metrics-nerel-part2-base.json
